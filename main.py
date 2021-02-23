25a3950d4e0f777fc90854f7e5275b419b4dffe3
Mira <mirakubai@gmail.com>
2021-02-23 16:21:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+set1 = {1,2,3}
+set2 = {2,3,5}
+a = set1.intersection(set2) #пересечение
+print(a)
+
+b = set1.union(set2) # обьединение
+print(b)
+
+c = set1.difference(set2) #разница
+print(c) 
+
+d = set1.symmetric_difference(set2) #симметр разница
+print(d)
